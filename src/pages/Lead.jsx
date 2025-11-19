0d365d5bc6ca87856fadf4b2c7e486821e21e34d
jeev-creator <aruvajeevitha@gmail.com>
2025-11-19 18:50:38 +0530

    leads modified

--- src/pages/Lead.jsx
+++ src/pages/Lead.jsx
@@ -94,7 +94,7 @@ const Lead = () => {
     const fetchLeads = async () => {
       try {
         setIsLoading(true);
-        const response = await api.get("/lead/not-converted");
+        const response = await api.get("/lead/non-converted");
         setLeads(response.data);
         const formattedData = response.data.map((group, index) => ({
           _id: group._id,
