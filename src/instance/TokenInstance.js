d92ec1a0c9543c5f2cb9d9825b54b40e7b76f3ae
grash2024 <grash2024@gmail.com>
2025-11-21 12:11:02 +0530

    react version updated

--- src/instance/TokenInstance.js
+++ src/instance/TokenInstance.js
@@ -2,7 +2,6 @@ import axios from "axios";
 const token = localStorage.getItem("Token");
 const api = axios.create({
     // baseUrl: "https://mychits.online/api",
-    // baseUrl: "http://13.61.139.208:3000/api",
     baseURL: "http://51.21.197.152:3000/api",
       // baseURL: "http://localhost:3000/api",
   headers: {
